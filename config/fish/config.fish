15c2e85f489717d8c27506b7cf9788a1d2b5a3b8
Gustav Bylund <maistho@gmail.com>
2017-09-25 17:37:30 +0200

    archington server config
    archington server config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -58,3 +58,11 @@ alias git-blame-count "git ls-tree --name-only -z -r HEAD|egrep -z -Z -E '\.(cc|
 
 alias nodepath "set PATH $PATH ./node_modules/.bin"
 
+if status --is-login
+    set PPID (echo (ps --pid %self -o ppid --no-headers) | xargs)
+    if ps --pid $PPID | grep ssh
+        tmux has-session -t remote; and tmux attach-session -t remote; or tmux new-session -s remote; and kill %self
+        echo "tmux failed to start; using plain fish shell"
+    end
+end
+
