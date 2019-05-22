4bb4345894abe7e96f4474a4215d6cafd447f757
Gustav Bylund <maistho@gmail.com>
2019-05-22 21:26:14 +0200

    add custom color theme

---
+++ config/fish/functions/bobthefish_colors.fish
@@ -0,0 +1,8 @@
+function bobthefish_colors -S -d 'Define a custom bobthefish color scheme'
+
+  __bobthefish_colors default
+
+  set -x color_username                 black blue --bold
+  set -x color_hostname                 black blue
+end
+
