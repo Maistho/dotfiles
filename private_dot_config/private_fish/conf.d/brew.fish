fa3b586cf353fbc9d67989909faf8363f3860696
Gustav Bylund <hello@gustavbylund.se>
2025-11-07 22:42:41 +0100

    add comment on cocoapods gnu binaries

--- private_dot_config/private_fish/conf.d/brew.fish
+++ private_dot_config/private_fish/conf.d/brew.fish
@@ -1,4 +1,6 @@
 if test -d /opt/homebrew
 	eval "$(/opt/homebrew/bin/brew shellenv)"
-	set -x PATH $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin $PATH
+    # I would like to use this but was running into issues with installing cocoapods...
+    # Comment out if you're getting odd issues
+    set -x PATH $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin $PATH
 end
